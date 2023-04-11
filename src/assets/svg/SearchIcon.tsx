75b154d3ee66a2db44f04c0cc6ba28d42de4f6ec
sunchaehoon <sunchaehoon@naver.com>
2023-04-11 10:02:25 +0900

    Add: SVG

---
+++ src/assets/svg/SearchIcon.tsx
@@ -0,0 +1,27 @@
+const SearchIcon = () => {
+  return (
+    <svg
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="13.0738"
+        cy="13.0738"
+        r="11.0738"
+        stroke="black"
+        stroke-width="4"
+      />
+      <path
+        d="M26.5 26.4999L21.7896 21.7896"
+        stroke="black"
+        stroke-width="4"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default SearchIcon;
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,10 @@
 export { default as DefaultProfileIcon } from './DefaultProfileIcon';
 export { default as NewTabLIcon } from './NewTabIcon';
 export { default as MenuIcon } from './MenuIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as AlermIcon } from './AlermIcon';
+export { default as ProfileIcon } from './ProfileIcon';
+export { default as ApplyIcon } from './ApplyIcon';
+export { default as Logo } from './Logo';
+export { default as LightMode } from './LightMode';
+export { default as DarkMode } from './DarkMode';
