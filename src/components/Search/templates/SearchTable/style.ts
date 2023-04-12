e1031a7af3fb4036b997a5084c8cf55b580dd9ad
Jeongmin <srm0609@naver.com>
2023-04-12 20:55:52 +0900

    Add: SearchTable

---
+++ src/components/Search/templates/SearchTable/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const TableWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+export const Header = styled.div`
+  font-weight: 700;
+  font-size: 28px;
+  border-bottom: 2px solid #eaeaea;
+  padding: 25px 0;
+  margin-bottom: 50px;
+`;
