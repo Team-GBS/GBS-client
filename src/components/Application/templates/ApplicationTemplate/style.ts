c566731890eac312f2f9572707c7bd16de6816a8
Jeongmin <srm0609@naver.com>
2023-04-13 13:43:59 +0900

    Add: ApplicationTemplate

---
+++ src/components/Application/templates/ApplicationTemplate/style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const ApplicationTemplate = styled.div`
+  width: 100%;
+  height: calc(100vh - 75px);
+  display: flex;
+  flex-direction: column;
+`;
