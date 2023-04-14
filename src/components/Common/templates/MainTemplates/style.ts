f34debf86f25267b36058af8befd19b3c0749f2e
sunchaehoon <sunchaehoon@naver.com>
2023-04-14 14:55:51 +0900

    Fix: Maintemplate

--- src/components/Common/templates/MainTemplates/style.ts
+++ src/components/Common/templates/MainTemplates/style.ts
@@ -2,9 +2,10 @@ import styled from '@emotion/styled';
 
 export const MainTemplates = styled.div`
   width: 1200px;
-  height: 100vh;
+  height: 100%;
   background: #ffffff;
   display: flex;
   flex-direction: column;
   margin: auto;
+  padding-bottom: 100px;
 `;
