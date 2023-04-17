624338f0f2ad74b305fed1cf15b65f9c2699a3d4
sunchaehoon <sunchaehoon@naver.com>
2023-04-17 09:39:04 +0900

    Add: MainPageTemplate

---
+++ src/components/Main/templates/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const HomeTemplate = styled.div`
+    width: 100%;
+    height: calc(100%-75px);
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 100px;
+`
+
