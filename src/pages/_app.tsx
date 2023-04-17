db97ffceeff1955fb053caeee743f0f5c0c2151d
sunchaehoon <sunchaehoon@naver.com>
2023-04-17 09:37:03 +0900

    Fix: _appPage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import '../styles/globals.tsx';
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globals';
 import '../../public/fonts/style.css';
+import { RecoilRoot } from 'recoil';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <ToastContainer />
+      </RecoilRoot>
     </>
   );
 }
