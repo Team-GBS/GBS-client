de78dcbf82b0d4a6f9b8f3326d28fa0983be0e36
선채훈 <sunchaehoon@naver.com>
2023-04-19 20:44:01 +0900

    Add: LoginPage

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import LoginPage from 'components/Login/organisms';
+import { LoginTemplate } from 'components/Login/templates/style';
+
+const Login = () => {
+  return (
+    <>
+      <LoginTemplate>
+        <LoginPage />
+      </LoginTemplate>
+    </>
+  );
+};
+
+export default Login;
