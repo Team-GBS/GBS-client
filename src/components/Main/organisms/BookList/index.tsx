0c3039d2a38fd82ebf0d758d623dc37046afb65e
Jeongmin <srm0609@naver.com>
2023-04-20 11:37:54 +0900

    Fix: BookList
    component

--- src/components/Main/organisms/BookList/index.tsx
+++ src/components/Main/organisms/BookList/index.tsx
@@ -1,32 +1,13 @@
-import BookBox from 'components/Main/atoms/BookBox';
 import * as S from './style';
-import { useState } from 'react';
+import CommoneBookList from 'components/Common/molecules/BookList';
 
 const BookList = () => {
-  const [booklists, setBooklists] = useState([0, 1, 2, 3, 4, 5]);
-  const [color, setColor] = useState({
-    red: '#FFF5F5',
-    yellow: '#FEFFF5',
-    skyblue: '#F5FBFF',
-    pink: '#FFF5FF',
-  });
-
   return (
     <>
       <S.Wrap>
         <S.TitleText>도서 목록</S.TitleText>
         <S.Container>
-          {booklists.map((booklist, i) => {
-            return (
-              <S.BookWrap key={i}>
-                <BookBox
-                  backgroundColor={color.pink}
-                  height="16.9rem"
-                />
-                <S.BookName>세상의 모든 과학</S.BookName>
-              </S.BookWrap>
-            );
-          })}
+          <CommoneBookList />
         </S.Container>
       </S.Wrap>
     </>
