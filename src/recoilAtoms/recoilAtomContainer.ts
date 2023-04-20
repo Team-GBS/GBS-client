6cda0ac651f870bbd7a75c375089d5dce5c787f3
Jeongmin <srm0609@naver.com>
2023-04-20 11:34:13 +0900

    Fix: recoil 위치 변경

--- src/recoilAtoms/recoilAtomContainer.ts
+++ src/recoilAtoms/recoilAtomContainer.ts
@@ -1,8 +1,6 @@
 import { atom } from 'recoil';
 
-const ShowModalAtom = atom<boolean>({
+export const ShowModalAtom = atom<boolean>({
   key: 'showModal',
   default: false,
 });
-
-export { ShowModalAtom };
