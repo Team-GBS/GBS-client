b07cb66de1e1db2402f8b3e1dfc532866a57d90a
Jeongmin <srm0609@naver.com>
2023-04-20 11:35:01 +0900

    Fix: pages import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { MainTemplates } from 'components/Common/templates/MainTemplates/style';
 import Main from 'components/Main/templates';
 
 const HomePage = () => {
   return (
-    <>
+    <MainTemplates>
       <Main />
-    </>
+    </MainTemplates>
   );
 };
 
