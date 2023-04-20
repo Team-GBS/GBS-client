d45bcd8a2c8d788b917b7860447287ac8c07176d
Jeongmin <srm0609@naver.com>
2023-04-20 11:36:58 +0900

    Fix: NewBook
    style, component

--- src/components/Main/organisms/NewBook/index.tsx
+++ src/components/Main/organisms/NewBook/index.tsx
@@ -1,8 +1,8 @@
-import { BookIcon, Logo, RightARrowIcon } from 'assets/svg';
+import { RightARrowIcon } from 'assets/svg';
 import * as S from './style';
 import LeftArrowIcon from 'assets/svg/LeftArrowIcon';
-import BookBox from 'components/Main/atoms/BookBox';
 import { useState } from 'react';
+import BookListItem from 'components/Common/atoms/BookListItem';
 
 const NewBook = () => {
   const [color, setColor] = useState({
@@ -16,25 +16,51 @@ const NewBook = () => {
       <S.Wrap>
         <S.TitleText>신간 도서</S.TitleText>
         <S.Container>
-          <BookBox backgroundColor={color.red} height='20.5rem' width='16.9rem' />
-          <S.BookInfo>
-            <S.BookTitle>[과학] 세상의 모든 과학</S.BookTitle>
-            <S.BookAuthor>Chun-ho-Yi</S.BookAuthor>
-            <S.BookCount>2권</S.BookCount>
-            <S.BookExplain>
-              150여 그림으로 펼쳐낸 가장 우아한 '빅 히스토리' 인기 팟캐스트
-              과빛밤 지기의 친절한 빅 히스토리 과학 여행서
-            </S.BookExplain>
-          </S.BookInfo>
-          <BookBox backgroundColor={color.yellow} height='20.5rem' width='16.9rem' />
-          <BookBox backgroundColor={color.skyblue} height='20.5rem' width='16.9rem' />
+          <S.BookInfoWrapper>
+            <BookListItem
+              title={'세상의 모든 과학'}
+              bookType={'900'}
+              state={'new'}
+            />
+            <S.BookInfo>
+              <S.BookTitle>[과학] 세상의 모든 과학</S.BookTitle>
+              <S.BookAuthor>Chun-ho-Yi</S.BookAuthor>
+              <S.BookCount>2권</S.BookCount>
+              <S.BookExplain>
+                150여 그림으로 펼쳐낸 가장 우아한 '빅 히스토리' 인기 팟캐스트
+                과빛밤 지기의 친절한 빅 히스토리 과학 여행서
+              </S.BookExplain>
+            </S.BookInfo>
+          </S.BookInfoWrapper>
+          <S.EBookList>
+            <BookListItem
+              title={'세상의 모든 과학'}
+              bookType={'500'}
+              state={'new'}
+            />
+            <BookListItem
+              title={'세상의 모든 과학'}
+              bookType={'500'}
+              state={'new'}
+            />
+            <BookListItem
+              title={'세상의 모든 과학'}
+              bookType={'500'}
+              state={'new'}
+            />
+            <BookListItem
+              title={'세상의 모든 과학'}
+              bookType={'500'}
+              state={'new'}
+            />
+          </S.EBookList>
         </S.Container>
-        <S.LeftArrow>
+        <S.Arrow state={true}>
           <LeftArrowIcon />
-        </S.LeftArrow>
-        <S.RightArrow>
+        </S.Arrow>
+        <S.Arrow state={false}>
           <RightARrowIcon />
-        </S.RightArrow>
+        </S.Arrow>
       </S.Wrap>
     </>
   );
