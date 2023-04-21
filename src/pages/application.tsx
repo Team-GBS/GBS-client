1b0073ed9787a326ad1be6138011ef175ca837e7
선채훈 <sunchaehoon@naver.com>
2023-04-21 22:57:49 +0900

    Fix: applicationPage

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -1,6 +1,6 @@
 import ApplicationTable from 'components/Application/templates/ApplicationTable';
 import { ApplicationTemplate } from 'components/Application/templates/ApplicationTemplate/style';
-import Header from 'components/Common/Header';
+import Header from 'components/Common/organisms/Header';
 import { MainTemplates } from 'components/Common/templates/MainTemplates/style';
 
 const ApplicationPage = () => {
