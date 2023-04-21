7824fc4b5a90c9da6630fbfd602acc8fdbab555d
선채훈 <sunchaehoon@naver.com>
2023-04-21 09:59:46 +0900

    Fix: BookListItem

--- src/components/Common/atoms/BookListItem/index.tsx
+++ src/components/Common/atoms/BookListItem/index.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
 import * as S from './style';
 import { BookItemProps } from 'types/Book';
 import { getBookColor } from 'Utils/Libs/getBookColor';
 import { NoneBookIcon } from 'assets/svg';
+import { useRecoilState } from 'recoil';
+import { ShowModalAtom } from 'components/Main/atoms/state';
 
-const BookListItem = ({ title, bookType, cover }: BookItemProps) => {
+const BookListItem = ({ title, bookType, cover, width, height, translateX }: BookItemProps) => {
+  const [showModal, setShowModal] = useRecoilState(ShowModalAtom);
+  const BookClick = () => {
+    setShowModal(!showModal);
+  };
   return (
-    <S.ItemWrapper>
-      <S.CoverWrapper bookType={getBookColor[bookType]}>
+    <S.ItemWrapper onClick={BookClick}>
+      <S.CoverWrapper bookType={getBookColor[bookType]} width={width} height={height} translateX={translateX}>
         {cover ? <></> : <NoneBookIcon />}
       </S.CoverWrapper>
       <S.Title>{title}</S.Title>
--- src/components/Common/atoms/BookListItem/style.ts
+++ src/components/Common/atoms/BookListItem/style.ts
@@ -1,23 +1,26 @@
 import styled from '@emotion/styled';
+import { BookItemProps } from 'types/Book';
 
 export const ItemWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 10px;
+  gap: 20px;
   cursor: pointer;
 `;
 
-export const CoverWrapper = styled.div`
-  width: 190px;
-  height: 270px;
+export const CoverWrapper = styled.div<BookItemProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   background-color: ${(props: { bookType: string }) => props.bookType};
+  transform: translateX(${(props) => props.translateX});
+  transition-duration: 0.3s;
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
-  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.04);
+  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.06);
   svg {
     width: 100px;
     height: 93px;
@@ -26,6 +29,7 @@ export const CoverWrapper = styled.div`
 
 export const Title = styled.div`
   font-size: 18px;
-  font-weight: 500;
+  font-weight: bold;
   color: #000000;
+  z-index: 5;
 `;
