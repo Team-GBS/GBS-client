e7873d098f5e1fe6a9413c8ef8821d660bab21b7
선채훈 <sunchaehoon@naver.com>
2023-04-21 10:00:59 +0900

    Fix: profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import Header from 'components/Common/Header';
+import Header from 'components/Common/organisms/Header';
 import { MainTemplates } from 'components/Common/templates/MainTemplates/style';
 import ProfileTable from 'components/Profile/templates/ProfileTable';
 import { ProfileTemplates } from 'components/Profile/templates/ProfileTemplate/style';
