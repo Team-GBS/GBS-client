a5d434bfbe679b2babf246b85fff435a648fbba5
선채훈 <sunchaehoon@naver.com>
2023-04-28 10:01:36 +0900

    Fix: BookListItem

--- src/components/Common/atoms/BookListItem/index.tsx
+++ src/components/Common/atoms/BookListItem/index.tsx
@@ -3,13 +3,31 @@ import { BookItemProps, BookItemStyleProps } from 'types/Book';
 import { getBookColor } from 'Utils/Libs/getBookColor';
 import { NoneBookIcon } from 'assets/svg';
 import { useRecoilState } from 'recoil';
-import { ShowModalAtom } from 'recoilAtoms/recoilAtomContainer';
+import {
+  ShowModalAtom,
+  ThisBookColorAtom,
+} from 'recoilAtoms/recoilAtomContainer';
+import { useEffect } from 'react';
 
-const BookListItem = ({ title, bookType, cover, state, translateX }: BookItemProps) => {
+const BookListItem = ({
+  title,
+  bookType,
+  cover,
+  state,
+  translateX,
+}: BookItemProps) => {
   const [showModal, setShowModal] = useRecoilState(ShowModalAtom);
+  const [thisBookColor, setThisBookColor] = useRecoilState(ThisBookColorAtom);
 
   return (
-    <S.ItemWrapper onClick={() => setShowModal(!showModal)} translateX={translateX} bookType=''>
+    <S.ItemWrapper
+      onClick={() => {
+        setShowModal(!showModal);
+        setThisBookColor(bookType);
+      }}
+      translateX={translateX}
+      bookType=""
+    >
       <S.CoverWrapper bookType={getBookColor[bookType]} state={state}>
         {cover ? <></> : <NoneBookIcon />}
       </S.CoverWrapper>
