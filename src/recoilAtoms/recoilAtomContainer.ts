8ce184a70c4a3f03806f3ef9c593c4539dd9d208
선채훈 <sunchaehoon@naver.com>
2023-04-28 10:01:20 +0900

    Add: recoil

--- src/recoilAtoms/recoilAtomContainer.ts
+++ src/recoilAtoms/recoilAtomContainer.ts
@@ -4,3 +4,8 @@ export const ShowModalAtom = atom<boolean>({
   key: 'showModal',
   default: false,
 });
+
+export const ThisBookColorAtom = atom<string>({
+  key: 'thisBookColor',
+  default: '',
+});
